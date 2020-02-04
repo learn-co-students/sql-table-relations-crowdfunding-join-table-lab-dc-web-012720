8872160785eb417bfe13c1803b0dbd87625e4b31
matteoaricci <matteo.a.ricci@gmail.com>
2020-02-04 16:34:00 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,66 @@
+INSERT INTO users (id, name, age) VALUES (1, "John", 21);
+INSERT INTO users (id, name, age) VALUES (2, "Jan", 22);
+INSERT INTO users (id, name, age) VALUES (3, "Joe", 24);
+INSERT INTO users (id, name, age) VALUES (4, "Jason", 26);
+INSERT INTO users (id, name, age) VALUES (5, "Jenna", 28);
+INSERT INTO users (id, name, age) VALUES (6, "Jorge", 30);
+INSERT INTO users (id, name, age) VALUES (7, "Jose", 32);
+INSERT INTO users (id, name, age) VALUES (8, "Joan", 34);
+INSERT INTO users (id, name, age) VALUES (9, "Jeanne", 36);
+INSERT INTO users (id, name, age) VALUES (10, "Janice", 38);
+INSERT INTO users (id, name, age) VALUES (11, "Jessa", 40);
+INSERT INTO users (id, name, age) VALUES (12, "Jen", 42);
+INSERT INTO users (id, name, age) VALUES (13, "Jennifer", 44);
+INSERT INTO users (id, name, age) VALUES (14, "Jeep", 46);
+INSERT INTO users (id, name, age) VALUES (15, "Jorp", 48);
+INSERT INTO users (id, name, age) VALUES (16, "Jackie", 50);
+INSERT INTO users (id, name, age) VALUES (17, "Jomp", 52);
+INSERT INTO users (id, name, age) VALUES (18, "Jinnn", 54);
+INSERT INTO users (id, name, age) VALUES (19, "Jornst", 56);
+INSERT INTO users (id, name, age) VALUES (20, "Jorkmpf", 58);
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+    (1, 'Building a PC', 'tech', 2000, '2013-02-02', '2013-08-02'),
+    (2, 'Tickets for Charly Bliss', 'music', 50, '2019-03-02', '2019-03-04'),
+    (3, 'New Phone', 'tech', 600, '2015-04-02', '2013-09-02'),
+    (4, 'Building a Stick', 'natural', 20, '2013-02-02', '2013-08-02'),
+    (5, 'Building a Tooth', 'natural', 200, '2013-02-02', '2013-08-05'),
+    (6, 'Building a Similar PC', 'tech', 2000, '2013-02-02', '2013-08-02'),
+    (7, 'Building a Better PC', 'tech', 20000, '2013-02-02', '2014-02-02'),
+    (8, 'Building a Void ', 'nothingness', 200000, '2013-02-02', '2015-08-02'),
+    (9, 'Building a Rollercoaster', 'tech', 2000000, '2013-02-02', '2014-12-02'),
+    (10, 'Building a Replica of Charly Bliss', 'Muisc', 20000000, '2013-02-02', '2020-08-02');
+    
+INSERT INTO pledges(id, amount, user_id, project_id) VALUES
+    (1, 200, 1, 1),
+    (2, 200, 2, 2),
+    (3, 200, 3, 3),
+    (4, 200, 4, 4),
+    (5, 200, 5, 5),
+    (6, 200, 6, 6),
+    (7, 200, 7, 7),
+    (8, 200, 8, 8),
+    (9, 200, 9, 9),
+    (10, 200, 10, 10),
+    (11, 200, 11, 1),
+    (12, 200, 12, 2),
+    (13, 200, 13, 3),
+    (14, 200, 14, 4),
+    (15, 200, 15, 5),
+    (16, 200, 16, 6),
+    (17, 200, 17, 7),
+    (18, 200, 18, 8),
+    (19, 200, 19, 9),
+    (20, 200, 20, 10),
+    (21, 200, 1, 1),
+    (22, 200, 12, 2),
+    (23, 200, 13, 3),
+    (24, 200, 14, 4),
+    (25, 200, 15, 5),
+    (26, 200, 16, 6),
+    (27, 200, 17, 7),
+    (28, 200, 18, 8),
+    (29, 200, 19, 9),
+    (30, 200, 10, 10);
+    
+    
